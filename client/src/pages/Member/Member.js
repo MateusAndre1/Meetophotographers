c3e9b227fb68a5b656f2ab51b1c314b35c490ea1
Andre Roque <mateusandre1@yahoo.com>
2019-11-02 18:04:15 -0400

    added another route to member

--- client/src/pages/Member/Member.js
+++ client/src/pages/Member/Member.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Photographer from "../Photographer";
 import Customer from "../Customer";
+import API from "../../utils/API";
 
 
 class Member extends Component {
@@ -14,23 +15,18 @@ class Member extends Component {
         };
     }
 
-
     componentDidMount() {
-        fetch("/api/user_data")
-            .then(res => {
-                return res.json();
-            }).then(data => {
-                if (data.userType === "Customer") {
-                    this.setState({
-                        isUser: true
-                    })
-                }
+        API.grabUser(this.state.firstName, this.state.userType).then((res) => {
+            if (res.data.userType === "Customer") {
                 this.setState({
-                    firstName: data.firstName,
-                    userType: data.userType,
+                    isUser: true
                 })
-            });
-
+            }
+            this.setState({
+                firstName: res.data.firstName,
+                userType: res.data.userType
+            })
+        })
     }
 
     render() {
