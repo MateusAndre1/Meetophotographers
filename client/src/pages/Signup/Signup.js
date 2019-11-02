c1bf09741a8b9bca5ecc433dd9cfd1ff180e6f59
Andre Roque <mateusandre1@yahoo.com>
2019-11-02 12:48:12 -0400

    added db schema

--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -20,7 +20,7 @@ class Signup extends Component {
     isAuthenticated(auth) {
         if (auth.status === 200) {
             sessionStorage.setItem('token', auth.config.data);
-            setTimeout(function () {window.location.href = "/members";}, 2000);
+            window.location.href = "/members";
             return false;
         }
     }
