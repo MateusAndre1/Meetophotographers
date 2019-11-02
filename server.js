e6b8a184fcdc972c4aff450d21a896aad9c29e62
Andre Roque <mateusandre1@yahoo.com>
2019-11-02 04:19:26 -0400

    reverted

--- server.js
+++ server.js
@@ -19,9 +19,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('*', (request, response) => {
-	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 var syncOptions = { force: false };
 
