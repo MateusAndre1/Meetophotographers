4e553d8bf2bf9c626c32f9f63be547cfada7a519
Andre Roque <mateusandre1@yahoo.com>
2019-11-02 03:51:33 -0400

    changed reload for forms

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
