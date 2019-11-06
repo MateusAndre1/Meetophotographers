f0a87b916e51e914adddbb4e70ceafd303155c90
Eric LaMacchia <ericlamacchia1@icloud.com>
2019-11-06 13:22:27 -0500

    Created jumbotron component

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import API from "../../utils/API";
 import Auth from "../../utils/auth";
+import Jumbotron from "../../components/Jumbotron"
 
 class Home extends React.Component {
     state = {
@@ -41,18 +42,9 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6 col-md-offset-3">
-                        <h1>Meetographer</h1>
-                    </div>
-                    {Auth.isUserAuthenticated() ? (
-                        <div>Logged in</div>
-                    ) : (
-                        <div>Not Logged in</div>
-                    )}
-                </div>
-            </div>
+            <>
+            <Jumbotron />
+            </>
         )
     }
 }
