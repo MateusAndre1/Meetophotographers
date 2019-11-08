3543db5477c720c9c76d46160e28bfeff2c9fab8
Andre Roque <mateusandre1@yahoo.com>
2019-11-07 23:09:14 -0500

    turned sync to false for heroku

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ if (process.env.NODE_ENV === 'production') {
 }
 
 
-var syncOptions = { force: true };
+var syncOptions = { force: false };
 
 // If running a test, set syncOptions.force to true
 // clearing the `testdb`
